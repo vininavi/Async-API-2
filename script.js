3c466d2f835b30d94b4dfcde3d5f4dd56b9eb799
vinithra <vinissb23698@gmail.com>
2024-09-15 23:54:54 +0530

    update

---
+++ script.js
@@ -0,0 +1,21 @@
+var request = new XMLHttpRequest()
+var url = "https://cataas.com/api/cats";
+request.open('GET',url,true)
+request.send()
+
+request.onload = function(){
+	if(request.status == 200 && request.readyState == 4){
+	var data = JSON.parse(this.response);	
+	console.log(data.message);
+	}
+	else{
+		alert("Page Status : "+request.status);
+		console.log("Page Status : "+request.status);
+	}
+	
+}
+
+request.onerror = function(){
+	console.log("connection failed")
+	alert("Connection failed");
+}
